65407e90f81c7eba29c85830c11eae39a16de4cd
Lucas Arroyo <lucas.aa68@gmail.com>
2023-07-04 11:29:30 -0300

    Add:mobile done

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3030;
+
+/* configuraciones */
+app.use(express.static(path.join(__dirname,'public')));
+
+/* rutas */
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/about',(req,res) => res.sendFile(path.join(__dirname, 'views','about.html')));
+app.get('/contact', (req,res) => res.sendFile(path.join(__dirname,'views','contact.html')));
+app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')));
+app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'views','404.html')));
+
+
+app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
